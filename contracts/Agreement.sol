6289725090096dda210852a5054f38eae2c25796
Akanksha <akanksha271@gmail.com>
2021-08-24 09:37:14 -0400

    updates

--- contracts/Agreement.sol
+++ contracts/Agreement.sol
@@ -15,10 +15,10 @@ contract Agreement {
     //uint256 expirydays;
     
     
-    address private employer;
-    address private employee;
+    address payable private employer;
+    address payable private employee;
     uint256 private contractamount;
-    mapping (address => bool) ConfirmationValues;
+    mapping (address => string) ConfirmationValues;
     // declare an event
     event ConfirmationEvent(address sendconfirmationto);
     event ResolutionEvent(address employee, address employer, string message);
@@ -27,8 +27,8 @@ contract Agreement {
 
 
     /**
-     * @dev triggers the confirmation for the task
-     * @param expirytime in number of days
+     * triggers the confirmation for the task
+     * expirytime in number of days
      
     async function triggersConfirmation(uint256 expirydays) public {
         const receipt = await eac.schedule({
@@ -40,17 +40,16 @@ contract Agreement {
         
     }*/
     
-    
-    constructor(address employeeAdr, uint256 amount) public {
+    constructor(address payable employeeAdr, uint256 amount) public {
       employee =   employeeAdr;
-      employer = msg.sender;
+      employer = payable(msg.sender);
       contractamount  = amount;
       assert (employer.balance > contractamount);
    }
    
-   function initiateContract external payable(){
+   function initiateContract () external payable{
        // need to transfer the contract and gas money
-      emit Transfer(employer, address(this), contractamount)
+      emit Transfer(employer, address(this), contractamount);
    }
     
 
@@ -58,36 +57,37 @@ contract Agreement {
         if (triggeraddress == employer) {
             if (completed) {
                 // trigger a transaction to employee
+                employee.transfer(contractamount);
+                emit Transfer(employer, employee, contractamount);
             }
             else if (retrieve(employee)){
-                employeeconfirmed = ConfirmationValues[employee]
-                    if (employeeconfirmed){
-                        emit ResolutionEvent(employee, employer, resMessage)
+                if (keccak256(bytes(ConfirmationValues[employee])) == keccak256(bytes("Completed"))){
+                        emit ResolutionEvent(employee, employer, resMessage);
                     }
                     else {
-                        empl
-                        emit Transfer(employer, employee, amount)
+                        employer.transfer(contractamount);
                     }
             }
             else{
-                emit ConfirmationEvent(employee)
+                emit ConfirmationEvent(employee);
             }
         }
         else if (triggeraddress == employee) {
             if (! completed){
                 // trigger transaction back to employer
+                employer.transfer(contractamount);
             }
             else if (retrieve(employer)){
-                employerconfirmed = ConfirmationValues[employer]
-                    if (employerconfirmed){
-                        emit Transfer(employer, employee, amount)
+                    if (keccak256(bytes(ConfirmationValues[employer])) == keccak256(bytes("Not Completed"))){
+                        employee.transfer(contractamount);
+                        emit Transfer(employer, employee, contractamount);
                     }
                     else {
-                        emit ResolutionEvent(employee, employer, resMessage)
+                        emit ResolutionEvent(employee, employer, resMessage);
                     }
             }
             else{
-                emit ConfirmationEvent(employer)
+                emit ConfirmationEvent(employer);
             }
         }
         
@@ -95,11 +95,13 @@ contract Agreement {
 
     /**
      * @dev Return value 
-     * @return value of 'number'
+     * @return if address confirmation present in the
      */
-    function retrieve() public view returns (address addressOfInterest){
-        if(bytes(ConfirmationValues[addressOfInterest]).length>0){
+    function retrieve(address addressOfInterest) public view returns (bool){
+        if(keccak256(bytes(ConfirmationValues[addressOfInterest])) == keccak256(bytes("Not Completed")) || 
+        keccak256(bytes(ConfirmationValues[addressOfInterest])) == keccak256(bytes("Completed"))){
             return true;
         }
+        return false;
     }
 }
